a219948840d355d993a3f5b4b206bec2c476a3a2
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-04-09 11:12:05 +0900

    different types of data

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+<body>
+  
+<?php
+    // string 
+     $name = 'coding is fun';
+
+    //  integer
+    $name = 20;
+
+    // float
+    $name = 20.5784;
+
+    // Boolean
+    // true = 1
+    // false = 0
+
+    // array
+    $names = array("Daniel","Dennis","Michael");
+    echo $names['0'];
+?>
+</body>
+</html> 
